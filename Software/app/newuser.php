c3c978ba84432eada74eb90615ea952dc46ea39e
bfarinasatwit <farinasb@wit.edu>
2023-03-16 07:41:07 -0400

    Deleted unused files, commented most of my code

--- Software/app/newuser.php
+++ Software/app/newuser.php
@@ -10,7 +10,9 @@ $txtFirstName = $_POST['firstname'];
 $txtLastName = $_POST['lastname'];
 $txtEmail = $_POST['email'];
 $txtPsswd = $_POST['password'];
-$hashedPsswd = password_hash($txtPsswd, PASSWORD_DEFAULT);
+//hashing the password to check the database
+//this was updated on 3/16/2023 to hash instead of password_hash
+$hashedPsswd = hash('sha256',$txtPsswd);
 // database insert SQL code
 $sql = "INSERT INTO `users` ( `firstName`, `lastName`, `email`, `user_pass`) VALUES ('$txtFirstName', '$txtLastName','$txtEmail', '$hashedPsswd')";
 
--- Software/app/signin.php
+++ Software/app/signin.php
@@ -1,1 +1,31 @@
 #This will be the php file that contacts the server and then validates data
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('db', 'root', 'password', 'Collectbiles4US');
+
+//retrieve data from webpage
+$txtEmail = $_POST['email'];
+$txtPsswd = $_POST['password'];
+//this was updated on 3/16/2023 to hash instead of password_hash
+$hashedPsswd = hash('sha256', $txtPsswd);
+//query to see if matching email and password exist in db
+$sql = "SELECT * FROM users WHERE email = '$txtEmail' AND user_pass = '$hashedPsswd'";
+$result = mysqli_query($con, $sql);
+//testers to see what the query returns and then to see all the global variable values
+var_dump($result);
+var_dump($GLOBALS);
+//actually checking to see if result is valid or not
+$check = mysqli_fetch_array($result);
+//if match send okay to controller to build view
+if (isset($check)) {
+    //just a tester remove when building webpage
+    echo 'Sucess!';
+}
+//else say incorrect email and password
+else {
+    //just a tester remove when building webpage
+    echo 'Faliure';
+}
+
+?>
