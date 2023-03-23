0731484a03f4e9f62290674538b36b75c971f3a0
bfarinasatwit <farinasb@wit.edu>
2023-03-23 11:09:55 -0400

    Added Carousel Form and changed up the init file

--- Software/node/src/Pages/HomePage.jsx
+++ Software/node/src/Pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Carousel from '../Components/carousel'
 import { Button as BootButton} from 'react-bootstrap'
 import { useLocation } from 'react-router-dom'
 const HomePage = () => {
     const location = useLocation();
     const userId = location.state.user_id;
     return (
-        <div>
-            {userId}
-        </div>
+        <>
+            <Carousel/>
+
+        </>
     )
 }
 
