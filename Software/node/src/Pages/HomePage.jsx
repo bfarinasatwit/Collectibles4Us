c64c27a26a11fc41e4634b5507471c6b1ece43d6
pancyckaatwit <54997253+pancyckaatwit@users.noreply.github.com>
2023-03-25 14:58:58 -0400

    Start of Header for homepage

--- Software/node/src/Pages/HomePage.jsx
+++ Software/node/src/Pages/HomePage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import CarouselComponent from '../Components/carousel'
 import { useLocation } from 'react-router-dom'
+import '../Styles/Header.css'
 const HomePage = () => {
     const location = useLocation();
     const userId = location.state.user_id;
     return (
         <>
+            <div className="Header">
+
+                <h1>
+                    Collectibles4Us
+                </h1>
+
+            </div>
             <CarouselComponent id={userId}/>
 
         </>
