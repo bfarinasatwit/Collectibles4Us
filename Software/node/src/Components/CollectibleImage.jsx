375a0a4626d17dd523967a930d6ced728584e240
hwalker145 <hwalker145@gmail.com>
2023-04-06 10:43:40 -0400

    New file, ready to start working to get collectible image

---
+++ Software/node/src/Components/CollectibleImage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CollectibleImage = () => {
+    return (
+        0
+    )
+}
+
+export default CollectibleImage
--- Software/node/src/Components/CollectiblePanel.jsx
+++ Software/node/src/Components/CollectiblePanel.jsx
@@ -3,9 +3,9 @@ import {Card as BootCard} from 'react-bootstrap'
 
 const CollectiblePanel = () => {
     return (
-        <div style={{"width": "160px"}}>
-            <BootCard>
-                
+        <div style={{ "width": "160px", "margin": "auto", "paddingTop": "20px" }}>
+            <BootCard style={{ "height": "240px" }}>
+
             </BootCard>
         </div>
     )
