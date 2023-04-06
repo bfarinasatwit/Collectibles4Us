d64768b15cdfd61a956f746eaf43085e81557385
hwalker145 <hwalker145@gmail.com>
2023-04-06 10:22:14 -0400

    Small change to Albumpanel

--- Software/node/src/Components/AlbumPanel.jsx
+++ Software/node/src/Components/AlbumPanel.jsx
@@ -21,11 +21,11 @@ const AlbumEditor = (props) => {
     return (
         <>
             <div style={{ "width": "160px", "margin": "auto", "paddingTop": "20px" }}>
-                <div className="shadow card" style={{ "height": "240px" }}>
+                <BootCard className="shadow" style={{ "height": "240px" }}>
                     <AlbumImage id={props.id} />
                     <BootCard.Title style={{ "fontSize": "16px" }}>{album.album_name}</BootCard.Title>
                     <BootCard.Body className='type' style={{ "fontSize": "16px", "padding": "10px" }}>{album.collect_type}</BootCard.Body>
-                </div>
+                </BootCard>
                 <div style={{ "display": "flex", "justifyContent": "center" }}>
                     <BootButton variant="dark" style={{ "margin": "8px", "fontSize": "12px" }}>
                         Remove
---
+++ Software/node/src/Components/CollectiblePanel.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CollectiblePanel = () => {
+    return (
+        0
+    )
+}
+
+export default CollectiblePanel
